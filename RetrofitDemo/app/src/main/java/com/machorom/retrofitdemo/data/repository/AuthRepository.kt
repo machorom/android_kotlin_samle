a343e7524cad42e3b11b44098f150919726f1ebb
kim dong hak <webmcr@daou.co.kr>
2020-02-10 18:46:13 +0900

    auth repository response 추가

--- RetrofitDemo/app/src/main/java/com/machorom/retrofitdemo/data/repository/AuthRepository.kt
+++ RetrofitDemo/app/src/main/java/com/machorom/retrofitdemo/data/repository/AuthRepository.kt
@@ -1,9 +1,10 @@
 package com.machorom.retrofitdemo.data.repository
 
 import com.machorom.retrofitdemo.data.datasource.remote.AuthRemoteDataSource
+import com.machorom.retrofitdemo.data.model.auth.LoginResponse
 
 class AuthRepository(private val authRemoteDataSource: AuthRemoteDataSource ) {
-    suspend fun login(userId:String, password:String ){
-        authRemoteDataSource.login(userId, password)
+    suspend fun login(userId:String, password:String ): LoginResponse {
+        return authRemoteDataSource.login(userId, password)
     }
 }
